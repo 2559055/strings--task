3b2709929af1087c3a42fc24f97fb285645c63f7
2559055 <moahmed.hussein@student.moringaschool.com>
2023-10-04 02:11:48 -0700

    strings task

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+const currentUser = "zaki";
+let welcomeMessage = "welcome to flatbook", ${currentUser}! 
+
+let excitedWelcomeMessage = "welcome to flatbook",${currentUser}!
+
+let shortGreeting = "welcome", ${currentUser}!
+
+
+
+
+
+
